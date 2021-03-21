4a3007e238a289ed7ebb7f77d36b07a3ef3ca851
Federico Rampazzo <federamp91@gmail.com>
2021-03-21 18:10:31 -0300

    Veteterinaria/Doctor

---
+++ Veterinaria.Consola/Veterinaria.Clases/Entidades/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Veterinaria.Clases.Entidades
+{
+    public class Doctor
+    {
+        private string nombre;
+        private string apellido;
+        private string especialidad;
+        private int matricula;
+
+        public Doctor(string nombre, string apellido, string especialidad, int matricula)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.especialidad = especialidad;
+            this.matricula = matricula;
+        }
+    }
+}
---
+++ Veterinaria.Consola/Veterinaria.Clases/Entidades/Veterinaria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Veterinaria.Clases
+{
+    public class Veterinaria
+    {
+        private string dueño;
+        private string direccion;
+        private string especialidad;
+        private float camillas;
+
+
+        public Veterinaria(string dueño, string direccion, string especialidad, float camillas)
+        {
+            this.dueño = dueño;
+            this.direccion = direccion;
+            this.especialidad = especialidad;
+            this.camillas = camillas;
+        }
+    }
+}
