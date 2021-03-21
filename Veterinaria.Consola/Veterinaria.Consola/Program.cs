14c1bfd0c7bd36d28f22571b7a5e19e415cff04f
Federico Rampazzo <federamp91@gmail.com>
2021-03-21 17:32:26 -0300

    Initial Code

---
+++ Veterinaria.Consola/Veterinaria.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Veterinaria.Consola
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
